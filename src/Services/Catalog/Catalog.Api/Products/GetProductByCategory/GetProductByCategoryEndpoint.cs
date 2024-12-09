c78c2c757941d63b81ded36c03e7f45269ac22d3
HJRumbo <53060263+HJRumbo@users.noreply.github.com>
2024-12-08 21:55:06 -0500

    Add project structure and create Catalog Api

---
+++ src/Services/Catalog/Catalog.Api/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.Api.Products.GetProductByCategory;
+
+//public record GetProductByCategoryRequest(Guid Id);
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (ISender sender, CancellationToken cancellationToken, string category) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category), cancellationToken);
+
+            var response = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(response);
+        })
+     .WithName("GetProductByCategory")
+     .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+     .ProducesProblem(StatusCodes.Status400BadRequest)
+     .WithSummary("Get Product By Category")
+     .WithDescription("Get Product By Category");
+    }
+}
